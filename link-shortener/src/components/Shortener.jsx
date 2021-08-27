0e1a17e8c0d599e0e50d7945e4eafd4698ff483c
magda watrak <magdawatrak9@gmail.com>
2021-08-27 11:51:25 +0200

    Added URL validation and copy button

--- link-shortener/src/components/Shortener.jsx
+++ link-shortener/src/components/Shortener.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+var validUrl = require('valid-url');
 
 function Shortener(){
 
@@ -12,10 +13,12 @@ function Shortener(){
     }
 
     function handleClick(event){
+        
         const inputLink=event.target.value;
         setLink(inputLink);
-    
-        fetch("https://api-ssl.bitly.com/v4/shorten", {
+
+        if (validUrl.isUri(link)){
+            fetch("https://api-ssl.bitly.com/v4/shorten", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -24,16 +27,30 @@ function Shortener(){
                 body: JSON.stringify({long_url: link})
             }).then(response => response.json())
             .then(data => setNewLink(data.link));
-    
             setLink(link);
+        } else {
+            setLink(link);
+            setNewLink("Imput is not an URL");
+        }
+    }
+
+    function copyToClipboard(){
+        var copyText = document.getElementById("newURL");
+
+        /* Select the text field */
+        copyText.select();
+
+        /* Copy the text inside the text field */
+        navigator.clipboard.writeText(copyText.value);
     }
 
     return(
         <div className="container">
             <p>Paste the link you want to shorten</p>
             <input placeholder="Your link" name="inputLink" onChange={handleChange} value={link}></input>
-            <button onClick={handleClick}>shorten</button>
-            <input placeholder="Shortened link" name="outputLink" disabled value={newLink}></input>
+            <button onClick={handleClick}>Shorten</button>
+            <input placeholder="Shortened link" name="outputLink" id="newURL" disabled value={newLink}></input>
+            <button id="copy" onClick={copyToClipboard}>Copy</button>
         </div>
 
     );
